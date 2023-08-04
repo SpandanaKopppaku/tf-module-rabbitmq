e71e76785a30a4382182c36894c9b6afd2d19c85
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 19:41:04 -0400

    Auto Push

--- route53.tf
+++ route53.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "rabbitmq" {
   name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  records = aws_spot_instance_request.allows_rabbitmq.private_ip
 }
 
 
