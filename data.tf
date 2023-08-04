cac3957205ee020c0c5d27bf4e6a5352b6912f1a
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 15:51:26 -0400

    Auto Push

--- data.tf
+++ data.tf
@@ -8,3 +8,9 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_ami" "image" {
+  most_recent      = true
+  name_regex       = "ansible-lab-image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
+  owners           = ["self"]
+}
