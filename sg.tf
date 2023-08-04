155fec60b434bb87d65a98b55364cd4c297acf21
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 19:47:02 -0400

    Auto Push

--- sg.tf
+++ sg.tf
@@ -11,6 +11,15 @@ resource "aws_security_group" "allows_rabbitmq" {
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
