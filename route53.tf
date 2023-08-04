3fc32f59c66283b09776b5926862e780170a1b3d
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 19:39:01 -0400

    Auto Push

---
+++ route53.tf
@@ -0,0 +1,12 @@
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.lb.public_ip]
+}
+
+
+
+
+
